d36bc400c494d1a87fd4ad5f41fe7db80d38efaf
Matt <ma.lepillan@duocuc.cl>
2025-04-29 11:07:28 -0400

    nuevos cambios

---
+++ src/main/java/ProgramacionWeb/demo/controller/PersonaController.java
@@ -0,0 +1,17 @@
+package ProgramacionWeb.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RequestMapping("/api/personas")
+@RestController
+public class PersonaController {
+    @GetMapping
+    public String saludar() {
+        return "La api funciona";
+    }
+    
+    
+}
